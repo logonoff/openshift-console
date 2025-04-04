66c711c8a25003b42173b0ddfb471ff88b53d9ed
logonoff <git@logonoff.co>
2025-04-04 10:27:02 -0400

    ODC-6675: Update jest
    - Switched from `ts-jest` to `esbuild-jest-transform` because `ts-jest` is slow
    - Updated `i18next-parser` and `esbuild-loader` to latest to avoid duplicate esbuild installations in yarn.lock
    - Removed `jest-resolver.js` because it was causing issues with resolving files
    - Jest config moved to `jest.config.ts` because a) it is recommended and b) so we can load `tsconfig` into the esbuild jest transformer

--- frontend/before-tests.js
+++ frontend/before-tests.js
@@ -1,56 +1,5 @@
 /* eslint-env node */
 
-import { URLSearchParams } from 'url';
-import fetch, { Headers } from 'node-fetch';
-
-// FIXME: Remove when jest is updated to at least 25.1.0 -- see https://github.com/jsdom/jsdom/issues/1555
-if (!Element.prototype.closest) {
-  Element.prototype.closest = function (this, selector) {
-    // eslint-disable-next-line consistent-this
-    let el = this;
-    while (el) {
-      if (el.matches(selector)) {
-        return el;
-      }
-      el = el.parentElement;
-    }
-    return null;
-  };
-}
-if (!Element.prototype.getRootNode) {
-  Object.defineProperty(Element.prototype, 'getRootNode', {
-    value: function () {
-      // eslint-disable-next-line consistent-this
-      let rootNode = this;
-      while (rootNode.parentNode) {
-        rootNode = rootNode.parentNode;
-      }
-      return rootNode;
-    },
-    writable: true,
-  });
-}
-// FIXME: Remove when jest is updated to at least 25
-if (!window.Headers) {
-  Object.defineProperty(window, 'Headers', {
-    value: Headers,
-    writable: true,
-  });
-}
-// FIXME: Remove when jest is updated to at least 22
-if (!window.URLSearchParams) {
-  Object.defineProperty(window, 'URLSearchParams', {
-    value: URLSearchParams,
-    writable: true,
-  });
-}
-if (!window.fetch) {
-  Object.defineProperty(window, 'fetch', {
-    value: fetch,
-    writable: true,
-  });
-}
-
 /**
  * Since Node.js 15 all unhandled promise rejections triggers a `unhandledRejection`
  * runtime event. If this event is not handled the process is automatically terminated.
